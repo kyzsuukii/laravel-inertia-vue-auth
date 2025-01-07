e2aa65446a5bf26ce7595b0febc6394670219eb2
Dwiky Rizky Ananditya <kyzsuki@gmail.com>
2025-01-07 20:38:30 +0700

    setup laravel intertia with vue
    Signed-off-by: Dwiky Rizky Ananditya <kyzsuki@gmail.com>

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3'
+</script>
+
+<template>
+    <Head title="Home" />
+    <div>
+        <h1>Welcome to your Inertia app!</h1>
+    </div>
+</template>
